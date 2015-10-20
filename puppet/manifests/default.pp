45b7c22a57bbf6a3c900e6da626ae55219ba1649
Mukund Krishnamurthy <mukund.kri@gmail.com>
2015-10-20 16:03:28 +0530

    Adding custom url for base image download

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,7 +1,14 @@
 class nodejs {
 
+  file { '/etc/apt/sources.list':
+    owner => root,
+    group => root,
+    ensure => present,
+    source => '/vagrant/puppet/modules/niit/sources.list',
+  }
   exec { 'run update script':
-    command => '/usr/bin/curl --silent --location https://deb.nodesource.com/setup_0.12 | /bin/bash -'
+    command => '/usr/bin/curl --silent --location https://deb.nodesource.com/setup_0.12 | /bin/bash -',
+    require => File['/etc/apt/sources.list'],
   }
   package { 'nodejs':
     ensure  => installed,
@@ -11,12 +18,10 @@ class nodejs {
     command => '/usr/bin/npm install -g node-static',
     require => Package['nodejs'],
   }
-
   file { '/etc/profile.d/000niitvars.sh':
     ensure => present,
     source => '/vagrant/puppet/modules/niit/000niitvars.sh',
   }
-
 }
 
 include nodejs
