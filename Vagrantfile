6a6b23defb3673030a32f1c15616ff949ebd26a2
Mukund Krishnamurty <mukund@eng.knowlarity.com>
2015-10-23 13:20:14 +0530

    Better comments on the puppet file

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@
 
 Vagrant.configure(2) do |config|
 
-  # Base bok related configuration.
+  # Base box related configuration.
   config.vm.box = "ubuntu/trusty32"
   config.vm.box_url = "http://172.23.238.13/vagrant/boxes/trusty-server-cloudimg-i386-vagrant-disk1.box"
   config.vm.box_check_update = false
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,11 +1,20 @@
 class nodejs {
 
+  # point apt to our apt-cacher for all packages
   file { '/etc/apt/sources.list':
     owner => root,
     group => root,
     ensure => present,
     source => '/vagrant/puppet/modules/niit/sources.list',
   }
+
+  # Custom system variables for niit vagrants
+  file { '/etc/profile.d/000niitvars.sh':
+    ensure => present,
+    source => '/vagrant/puppet/modules/niit/000niitvars.sh',
+  }
+
+  # Install node as recomended by nodejs.org
   exec { 'run update script':
     command => '/usr/bin/curl --silent --location https://deb.nodesource.com/setup_0.12 | /bin/bash -',
     require => File['/etc/apt/sources.list'],
@@ -14,14 +23,13 @@ class nodejs {
     ensure  => installed,
     require  => Exec['run update script'],
   }
+
+  # Install node-static, a simple static web server wirtten with node
   exec { 'install node static':
     command => '/usr/bin/npm install -g node-static',
     require => Package['nodejs'],
   }
-  file { '/etc/profile.d/000niitvars.sh':
-    ensure => present,
-    source => '/vagrant/puppet/modules/niit/000niitvars.sh',
-  }
+
 }
 
 include nodejs
